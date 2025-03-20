6b8f41974b434dfbf6ee50a572fdbc7a4684cb3e
Zarni Hlawn <zarnihlawn@outlook.com>
2025-03-20 16:08:50 +0630

    added hostname to device_info_znl

--- device_info_znl/src/lib.rs
+++ device_info_znl/src/lib.rs
@@ -1,13 +1,2 @@
 pub mod modules;
 
-#[cfg(test)]
-mod tests {
-    use crate::modules::hostname::get_hostname;
-
-    #[test]
-    fn test_get_hostname() {
-        let hostname = get_hostname();
-        println!("Hostname: {}", hostname);
-        assert!(!hostname.is_empty(), "Hostname should not be empty");
-    }
-}
